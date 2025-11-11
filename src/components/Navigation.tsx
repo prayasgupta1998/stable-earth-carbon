a5ae07e2fcae67b2f7017d93529104c71f004a9b
prayasgupta1998 <prayas.tension@gmail.com>
2025-11-11 12:04:34 +0530

    cursor changes - logo size

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -21,7 +21,7 @@ const Navigation = () => {
       <div className="container mx-auto px-4">
         <div className="flex items-center justify-between h-20">
           <Link to="/" className="flex items-center">
-            <img src={logo} alt="SthirCarbon" className="h-15 w-auto" />
+            <img src={logo} alt="SthirCarbon" className="h-[2.66rem] w-auto" />
           </Link>
 
           {/* Desktop Navigation */}
